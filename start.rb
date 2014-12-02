0c0f6f3b03bc70343ddbfa10061287334f7ee447
noviantonugroho <noviantonugroho1989@gmail.com>
2014-12-02 12:46:08 +0700

    this is the battle start class

---
+++ start.rb
@@ -0,0 +1,13 @@
+class start
+
+#require "./player"
+	def initialize
+		puts "#=====================================================#"
+		puts "#                 Welcome to Battle Arena             #"
+		puts "#-----------------------------------------------------#"
+		puts "#   Battle Start :                                    #"
+		puts "#   siapa yang akan menyerang : #{@player1|@player2}  #"
+		puts "#   siapa yang diserang: #{@player1|@player2}         #"
+		puts "#=====================================================#"
+	end
+end
